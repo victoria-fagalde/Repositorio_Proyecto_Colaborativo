d2d4175ec32c54144f8414ad6a46cc6101df86c5
delfinapuiggari <dpuiggari@udesa.edu.ar>
2026-04-05 22:10:33 -0300

    Update procesamiento_datos.py

--- src:/procesamiento_datos.py
+++ src:/procesamiento_datos.py
@@ -1,8 +1,22 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+def filtrar_por_participante(datos: list, id_participante: int) -> dict:
 """
-Created on Sun Apr  5 21:43:44 2026
+Selecciona los datos correspondientes a un susario/participante 
+Busca dentro de la lista el diccionario cuya id_participante coincida
 
-@author: vickyfagalde
+Parametros:
+datos: list
+lista de diccionarios a filtrar
+
+id_participante: int
+numero que identifica al usuario, clave del diccionario
+
+Retorna:
+dict: el diccionario del paticipante encontrado
+None: si no se encuentra el participante
 """
-
+    
+  for diccionario in datos:
+    if id "id_participante" in diccionario: 
+      if diccionario["id_participante"]==id_participante: 
+        return diccionario
+  return None
